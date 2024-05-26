ae91904dc08b0bc6010c6dbd991ca3c096151f20
Marlysson Silva Dantas <124621572+MarlyssonSD@users.noreply.github.com>
2024-05-26 14:15:56 -0300

    Delete MUNDO 3 directory

--- MUNDO 3/096 - Função que calcula área.py
+++
@@ -1,8 +0,0 @@
-def area(larg, compr):
-    print(f'A área de um terreno {larg}x{compr} é: {larg*compr}m²')
-
-
-largura = float(input("Digite a largura (m): "))
-comprimento = float(input("Digite o comprimento (m): "))
-
-area(largura, comprimento)
--- MUNDO 3/097 - Um print especial.py
+++
@@ -1,9 +0,0 @@
-def escreva(frase):
-    print('~'*(len(frase)+4))
-    print(f'  {frase}')
-    print('~'*(len(frase)+4))
-
-
-escreva('Apenas um teste')
-escreva('Será que ficará exatamente no meio?')
-
